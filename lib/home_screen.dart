5faea2b72430c887044d3e3845302c35efcd09f7
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-07 15:08:38 +0500

    committed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,19 +11,35 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
+        length: 4,
         child: Scaffold(
             appBar: AppBar(
+              backgroundColor: const Color(0xff435A64),
               title: const Text('Whatsapp'),
-              centerTitle: true,
+              centerTitle: false,
               bottom: const TabBar(
                 tabs: [
-                  Text('Chats'),
-                  Text('Status'),
-                  Text('Calls'),
+                  Tab(child: Icon(Icons.people)),
+                  Tab(child: Text('Chats')),
+                  Tab(child: Text('Status')),
+                  Tab(child: Text('Calls')),
                 ],
               ),
+              actions: const [
+                Icon(Icons.search),
+                SizedBox(width: 10,),
+                Icon(Icons.more_vert),
+                SizedBox(width: 10,),
+              ],
             ),
+          body: const TabBarView(
+            children: [
+              Text('Community'),
+              Text('Chats'),
+              Text('Status'),
+              Text('Calls'),
+            ],
+          ),
         )
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
