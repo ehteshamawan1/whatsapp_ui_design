8676c7fcd9f93f2afed739eed9582b1a409f1739
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-07 17:33:55 +0500

    committed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,25 +13,56 @@ class _HomeScreenState extends State<HomeScreen> {
     return DefaultTabController(
         length: 4,
         child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: const Color(0xff435A64),
-              title: const Text('Whatsapp'),
-              centerTitle: false,
-              bottom: const TabBar(
-                tabs: [
-                  Tab(child: Icon(Icons.people)),
-                  Tab(child: Text('Chats')),
-                  Tab(child: Text('Status')),
-                  Tab(child: Text('Calls')),
-                ],
-              ),
-              actions: const [
-                Icon(Icons.search),
-                SizedBox(width: 10,),
-                Icon(Icons.more_vert),
-                SizedBox(width: 10,),
+          appBar: AppBar(
+            backgroundColor: const Color(0xff273443),
+            title: const Text('WhatsApp'),
+            centerTitle: false,
+            bottom: const TabBar(
+              tabs: [
+                Tab(child: Icon(Icons.people)),
+                Tab(child: Text('Chats')),
+                Tab(child: Text('Status')),
+                Tab(child: Text('Calls')),
               ],
             ),
+            actions: [
+              const Icon(Icons.camera_alt_outlined),
+              const SizedBox(
+                width: 20,
+              ),
+              const Icon(Icons.search),
+              const SizedBox(
+                width: 20,
+              ),
+              PopupMenuButton(
+                itemBuilder: (context) => [
+                  const PopupMenuItem(
+                    value: 1,
+                    child: Text('New group'),
+                  ),
+                  const PopupMenuItem(
+                    value: 2,
+                    child: Text('New broadcast'),
+                  ),
+                  const PopupMenuItem(
+                    value: 3,
+                    child: Text('Linked devices'),
+                  ),
+                  const PopupMenuItem(
+                    value: 4,
+                    child: Text('Starred messages'),
+                  ),
+                 const PopupMenuItem(
+                    value: 5,
+                    child: Text('Settings'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
           body: const TabBarView(
             children: [
               Text('Community'),
@@ -40,7 +71,6 @@ class _HomeScreenState extends State<HomeScreen> {
               Text('Calls'),
             ],
           ),
-        )
-    );
+        ));
   }
 }
